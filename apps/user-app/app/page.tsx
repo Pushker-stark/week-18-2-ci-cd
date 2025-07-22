578a360c861fd1fffbdfff0bc7e22882a9799ea2
Pushker Naresh <pushkern@amazon.com>
2025-07-22 22:36:44 +0530

    brach file updated

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -7,6 +7,9 @@ import { getServerSession } from "next-auth";
 import { redirect } from 'next/navigation'
 import { authOptions } from "./lib/auth";
 
+
+
+
 export default async function Page() {
   const session = await getServerSession(authOptions);
   if (session?.user) {
